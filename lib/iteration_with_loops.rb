58a2e784214a8459dce66d3a12844a6aebc702cd
DJ Draper <darcyjdraper@gmail.com>
2020-07-23 03:16:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,13 @@
 def join_nested_strings(src)
   final_string = ""
-  row_indec = 0 
+  row_index = 0 
+  while row_index < src.count do 
+    element_index = 0 
+    while element_index < src[row_index][element_index] + ''
+  end
+  element_index += 1 
+end
+row_index += 1 
+end 
+final_string
 end
